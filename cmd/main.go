556e5701b86c76f5919840f78883288380b08472
Dnreikronos <dnreikronos2@gmail.com>
2025-03-03 20:53:10 -0300

    feature: load .env with godotenv

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,16 @@
 package main
 
-import connection "github.com/Dnreikronos/image_resizer_b/db"
+import (
+	connection "github.com/Dnreikronos/image_resizer_b/db"
+	"github.com/joho/godotenv"
+)
 
+func main() {
 
-
-
-func main () {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
 
 	db, err := connection.OpenConnection()
 	if err != nil {
