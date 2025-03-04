5bec5d2b87b47ffe37ef53df1518c91eea05cb04
Dnreikronos <dnreikronos2@gmail.com>
2025-03-03 21:29:36 -0300

    feature: create image model file

--- models/image.go
+++ models/image.go
@@ -1,8 +1,16 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type Image struct {
 	ID       uuid.UUID   `gorm:"primaryKey"`
 	Filename string `gorm:"not null"`
 	Data     []byte `gorm:"not null"`
 }
+
 func (i *Image) BeforeCreate(d *gorm.DB) (err error) {
 	i.ID = uuid.New()
 	return
