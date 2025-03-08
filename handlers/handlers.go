e9e12e8deef508d2baa5779ab631c307fdcca2a0
Dnreikronos <dnreikronos2@gmail.com>
2025-03-07 21:23:10 -0300

    feature: validate file foramt

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,3 +3,9 @@
 		c.JSON(http.StatusBadRequest, gin.H{"error": "File is required"})
 		return
 	}
+	defer file.Close()
+
+	if err := utils.ValidateFile(fileHeader); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
